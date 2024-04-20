6fcb4e2b02748b168eaee7c98526785c1e7ed326
Skeletonman59 <54837039+Skeletonman59@users.noreply.github.com>
2024-04-19 20:10:56 -0700

    Commit 2: Final Menu Changes
    Added keyboard support

---
+++ project_deadrunner/include/Screen.h
@@ -0,0 +1,32 @@
+#ifndef SCREEN_H
+#define SCREEN_H
+
+#include<common.h>
+#include<GLTexture.h>
+#include<GLTimer.h>
+
+class Screen
+{
+    public:
+        Screen();
+
+        GLTexture *bg = new GLTexture();
+        GLTimer   *myTime = new GLTimer();
+        virtual ~Screen();
+
+        void screenInit(float, float, float, float, char*);        // load background file
+        void screenDraw(float, float);  // width and height
+        float xMax,xMin,yMax,yMin,zPos,xPos, yPos;
+        float alphaIter, alpha;
+
+        enum{NOTHING, FADEIN, FADEOUT, HELPIN, HELPOUT};
+        void actions();
+        int screenTrigger;
+        bool menuLock;  //prevents all actions from being done at once.
+
+    protected:
+
+    private:
+};
+
+#endif // SCREEN_H
